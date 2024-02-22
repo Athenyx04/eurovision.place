1a4b98192882e23cda0744bed8d8976b7c709155
Manuel Calvo <manucalvom@usal.es>
2024-02-22 01:01:11 +0100

    feat: GitHub actions with ESLint, Vitest, Playwright and Vercel

---
+++ tests/e2e/index.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from "@playwright/test";
+
+test("index page has correct title and renders cards", async ({ page }) => {
+  await page.goto("/");
+
+  // Check for main elements
+  await expect(page).toHaveTitle(/Welcome to Astro/);
+  await expect(page.locator("main > h1")).toContainText("Welcome to Astro");
+  await expect(page.locator(".link-card-grid")).toBeVisible();
+
+  // Check for a specific card
+  const card = page.locator('.link-card:has-text("Documentation")');
+  await expect(card).toBeVisible();
+  await expect(card).toContainText(
+    "Learn how Astro works and explore the official API docs."
+  );
+});
---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { getViteConfig } from "astro/config";
+import type { UserConfig } from "vitest";
+
+export default getViteConfig({
+  test: {
+    // Vitest configuration options
+    globals: true,
+    environment: "jsdom",
+  },
+});
