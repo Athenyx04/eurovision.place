d6a76f88824b1e5491b97a7b8d9a277c20006890
Manuel Calvo <manucalvom@usal.es>
2024-03-04 01:34:14 +0100

    fix(e2e-tests): page title assertion

--- test/e2e/index.spec.ts
+++ test/e2e/index.spec.ts
@@ -1,8 +1,8 @@
-import { test, expect } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 test('index page has correct title', async ({ page }) => {
   await page.goto('/')
 
   // Check for main elements
-  await expect(page).toHaveTitle(/Sorter | Eurovision Place/)
+  await expect(page).toHaveTitle(/Eurovision Place/)
 })
