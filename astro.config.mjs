5b2602bb29671c35d96b4e73c029803b4b188dcb
Manuel Calvo <manucalvom@usal.es>
2024-03-11 00:35:16 +0100

    feat: add Vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
-
 import react from '@astrojs/react'
+
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
   adapter: vercel({
-    webAnalytics: { enabled: true }
+    webAnalytics: {
+      enabled: true
+    }
   })
 })
 //
