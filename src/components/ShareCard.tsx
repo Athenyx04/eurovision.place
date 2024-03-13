cf0ab41c39dc137a05f293870bd7bec203a7d001
Manuel Calvo <manucalvom@usal.es>
2024-03-13 14:47:20 +0100

    feat: changes to ranking (temporary)

--- src/components/ShareCard.tsx
+++ src/components/ShareCard.tsx
@@ -19,18 +19,16 @@ function ShareCard({
     <div
       className={`flex min-w-0 select-none ${
         position === 1
-          ? 'col-span-2 bg-yellow-200 text-black'
+          ? 'border-t-2 border-liberty bg-[#ffcf40] text-eerie'
           : position === 2
-            ? 'border-t-2 border-platinum bg-gray-400'
+            ? 'border-t-2 border-platinum bg-gray-400 text-eerie'
             : position === 3
-              ? 'border-t-2 border-platinum bg-orange-900'
+              ? 'border-t-2 border-platinum bg-[#c96f2a] text-eerie'
               : 'border-t-2 border-liberty bg-eerie'
-      }`}
+      } ${gridColumn === 1 ? 'border-r-4 border-r-liberty' : ''}`}
       style={{ gridColumn, gridRowStart }}
     >
-      <div
-        className={`relative shrink-0 ${position === 1 && gridColumn !== 'span 1 / span 1' ? 'w-[10%]' : 'w-1/5'}`}
-      >
+      <div className={`relative shrink-0 w-1/5`}>
         <img
           className='absolute left-0 top-0 size-full object-cover'
           src={artist.imageUrl}
@@ -38,10 +36,21 @@ function ShareCard({
         />
         <div className='pb-[100%}'></div>
       </div>
-      <div className='flex size-8 shrink-0 -translate-x-4 items-center justify-center self-center rounded-full bg-liberty'>
-        <span className='font-bold text-slate-200'>
-          {String(position).padStart(2, '0')}
-        </span>
+      <div
+        className={`flex size-8 shrink-0 -translate-x-4 items-center justify-center self-center rounded-full shadow-lg ${position === 1 ? 'bg-[#ffdc73] text-eerie' : position === 2 ? 'bg-gray-300 text-eerie' : position === 3 ? 'bg-[#e5703c] text-eerie' : 'bg-liberty'}`}
+        style={{
+          backgroundImage: `linear-gradient(
+            30deg,
+            transparent 20%,
+            transparent 40%,
+            rgba(255, 255, 255, ${position === 1 ? '0.8' : position === 2 ? '0.8' : position === 3 ? '0.4' : '0.25'}) 50%,
+            rgba(255, 255, 255, ${position === 1 ? '0.8' : position === 2 ? '0.8' : position === 3 ? '0.4' : '0.25'}) 55%,
+            transparent 70%,
+            transparent 100%
+          )`
+        }}
+      >
+        <span className='font-bold'>{String(position).padStart(2, '0')}</span>
       </div>
       <div className='flex min-w-0 grow flex-row items-center py-3'>
         <div className='flex min-w-0 grow flex-col'>
--- src/components/ShareContainer.tsx
+++ src/components/ShareContainer.tsx
@@ -13,15 +13,11 @@ function ShareContainer({
   title: string
 }) {
   const headerHeight = 40
-  const firstSongCardHeight = 48
   const songCardHeight = 50
 
-  const numRows =
-    songs.length < 11 ? songs.length : Math.ceil((songs.length - 1) / 2)
+  const numRows = songs.length < 11 ? songs.length : Math.ceil(songs.length / 2)
   const height =
-    songs.length < 11
-      ? 'auto'
-      : headerHeight + firstSongCardHeight + songCardHeight * numRows
+    songs.length < 11 ? 'auto' : headerHeight + songCardHeight * numRows
   const width = height === 'auto' ? 400 : height < 400 ? 400 : height
   const gridTemplateColumns =
     songs.length < 11
@@ -68,7 +64,7 @@ function ShareContainer({
     <div
       id={id}
       className={
-        'absolute left-[-9999px] grid grid-cols-1 overflow-hidden bg-eerie'
+        'absolute left-[-0px] grid grid-cols-1 overflow-hidden bg-eerie'
       }
       style={{
         height,
@@ -85,7 +81,7 @@ function ShareContainer({
       </div>
 
       {/* Render the first song across both columns */}
-      {songs.slice(0, 1).map((song) => (
+      {/* {songs.slice(0, 1).map((song) => (
         <ShareCard
           artist={song.artist}
           country={song.country}
@@ -93,10 +89,10 @@ function ShareContainer({
           key={song.id}
           gridColumn='1 / -1'
         />
-      ))}
+      ))} */}
 
       {/* Render the second and third song in separate columns */}
-      {songs.slice(1, 3).map((song, index) => (
+      {/* {songs.slice(1, 3).map((song, index) => (
         <ShareCard
           artist={song.artist}
           country={song.country}
@@ -105,30 +101,71 @@ function ShareContainer({
           // Position them in the grid based on their index
           gridColumn={index + 1}
         />
-      ))}
+      ))} */}
 
-      {/* For songs 4 to half of the rest (left column) */}
-      {songs.slice(3, songs.length / 2 + 2).map((song, index) => (
+      {/* For songs 1 to half (left column) */}
+      {songs.slice(0, Math.ceil(songs.length / 2)).map((song, index) => (
         <ShareCard
           artist={song.artist}
           country={song.country}
-          position={index + 4}
+          position={index + 1}
           key={song.id}
           gridColumn={1}
         />
       ))}
 
-      {/* For half to end of the rest (right column) */}
-      {songs.slice(songs.length / 2 + 2).map((song, index) => (
+      {/* For half to end (right column) */}
+      {songs.slice(Math.ceil(songs.length / 2)).map((song, index) => (
         <ShareCard
           artist={song.artist}
           country={song.country}
-          position={Math.ceil(songs.length / 2) + index + 2}
+          position={Math.ceil(songs.length / 2) + index + 1}
           key={song.id}
           gridColumn={2}
-          gridRowStart={index + 4}
+          gridRowStart={index + 2}
         />
       ))}
+
+      {/* Add a white box if the number of songs is odd */}
+      {songs.length % 2 !== 0 && (
+        <div
+          className='bg-slate-200 border-t-2 border-liberty flex justify-center items-center gap-2'
+          style={{
+            gridColumn: 2,
+            gridRowStart: Math.ceil(songs.length / 2) + 1,
+            height: `${songCardHeight}px`, // Assuming this is the height of your ShareCard
+            width: '100%' // Assuming ShareCard takes up 100% of the column width
+          }}
+        >
+          {/* <svg
+            viewBox='0 0 24 24'
+            aria-hidden='true'
+            width='24'
+            height='24'
+            astro-icon='social/twitter'
+          >
+            <path
+              fill='#000'
+              d='M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z'
+            ></path>
+          </svg> */}
+          <span className='font-bold text-sm text-eerie flex gap-[0.08rem]'>
+            made with{' '}
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='currentColor'
+              className='scale-90'
+            >
+              <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+              <path d='M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z' />
+            </svg>{' '}
+            at eurovision.place
+          </span>
+        </div>
+      )}
     </div>
   )
 }
